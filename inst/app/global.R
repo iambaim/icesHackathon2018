df9a87278f747086121af681b20ca04155ae3576
Copepoda <Nissa.Ferm@gmail.com>
2018-05-30 09:44:41 +0200

    Added function to create density plot
    Calculates historical density for using the station destination which corresponds to the haul which is brushed on the K plot.

--- inst/app/global.R
+++ inst/app/global.R
@@ -40,3 +40,31 @@ getSpeciesList <- function(haulSubsetType){
   return(species)
 }
 
+
+Brush_densityplot <- function(This_year, Fish_choice,Current_Station, Last_haul,fish_data){
+  #Last Haul refers to the brush point from the K plot. 
+density_histor <- fish_data %>% filter(Year != This_year)%>%
+  filter(SpecCodeType == Fish_choice)%>% #Fish selection var,
+  filter(StNo == Current_Station)#Current Station to match current haul location
+
+density_histor <- as.data.frame(rep(density_histor$LngtClass, density_histor$HLNoAtLngt))
+colnames(density_histor) <- "den_hist"
+
+density_curr <- fish_data %>% filter(Year == This_year)%>%
+  filter(SpecCodeType == Fish_choice)%>%
+  filter(HaulNo == Last_haul)
+
+density_curr <- as.data.frame(rep(density_curr$LngtClass, density_curr$HLNoAtLngt))
+colnames(density_curr) <- "den_curr"
+
+
+ggplot()+
+  #historical density distribution for selected or last station
+  geom_density(aes(x = den_hist),color = Hist_color, size = 2,data = density_histor )+
+  #current density distribution for selected or last station 
+  geom_density(aes(x = den_curr ), color = Current_color,size = 2,
+                 alpha = 0.6, data = density_curr)+
+  xlab(label = "Length Class (mm)")+
+  theme_bw()
+
+}
